61a127c6a7e99a9bd558c3a30f402c17ccfa6f95
Kassio Borges <kassioborgesm@gmail.com>
2021-01-10 16:43:44 +0000

    Add solargraph back, remove vint

--- lua/my/lspconfig.lua
+++ lua/my/lspconfig.lua
@@ -16,116 +16,112 @@ lspconfig.vimls.setup{ on_attach = attacher }
 lspconfig.jsonls.setup{ on_attach = attacher }
 lspconfig.cssls.setup{ on_attach = attacher }
 
-lspconfig.diagnosticls.setup{
-  filetypes = {
-    'markdown',
-    'ruby',
-    'sh',
-    'vim',
-    'yaml'
-  },
-
-  init_options = {
-    linters = {
-      rubocop = {
-        command = 'bundle',
-        sourceName = 'rubocop',
-        args = {
-          'exec',
-          'rubocop',
-          '--format',
-          'json',
-          '--force-exclusion',
-          '%filepath'
-        },
-        parseJson = {
-          errorsRoot = 'files[0].offenses',
-          line = 'location.line',
-          column = 'location.column',
-          message = '[${cop_name}]\n${message}',
-          security = 'severity'
-        },
-        securities = {
-          fatal = 'error',
-          warning = 'warning'
-        }
-      },
-      markdownlint = {
-        command = 'markdownlint',
-        sourceName = 'markdownlint',
-        isStderr = true,
-        args = {
-          '--config',
-          '.markdownlint.json',
-          '%filepath'
-        },
-        formatLines = 1,
-        formatPattern = {
-          '^([^:]+):(\\d+):(\\d+)?\\s*(.*)$',
-          {
-            sourceName = 1,
-            sourceNameFilter = true,
-            line = 2,
-            column = 3,
-            message = 4,
-          }
-        },
-        securities = {
-          undefined = 'warning'
-        }
-      },
-      shellcheck = {
-        command = 'shellcheck',
-        debounce = 100,
-        args = {
-          '--format',
-          'json',
-          '-'
-        },
-        sourceName = 'shellcheck',
-        parseJson = {
-          line = 'line',
-          column = 'column',
-          endLine = 'endLine',
-          endColumn = 'endColumn',
-          message = '${message} [${code}]',
-          security = 'level'
-        },
-        securities = {
-          error = 'error',
-          warning = 'warning',
-          info = 'info',
-          style = 'hint'
-        }
-      },
-      vint = {
-        command = 'vint',
-        debounce = 100,
-        args = {
-          '--enable-neovim',
-          '-'
-        },
-        offsetLine = 0,
-        offsetColumn = 0,
-        sourceName = 'vint',
-        formatLines = 1,
-        formatPattern = {
-          '[^:]+:(\\d+):(\\d+):\\s*(.*)(\\r|\\n)*$',
-          {
-            line = 1,
-            column = 2,
-            message = 3
-          }
-        }
-      }
-    },
-
-    filetypes = {
-      markdown = 'markdownlint',
-      ruby = 'rubocop',
-      sh = 'shellcheck',
-      vim = 'vint',
+lspconfig.solargraph.setup{
+  settings = {
+    solargraph = {
+      completion = true,
+      definitions = false,
+      diagnostics = false,
+      filetypes = {'ruby'},
+      hover = false,
+      references = false,
+      rename = false,
+      root_dir = {'.'},
+      symbols = false,
+      useBundler = false
     }
   },
   on_attach = attacher
 }
+
+  lspconfig.diagnosticls.setup{
+    filetypes = {
+      'markdown',
+      'ruby',
+      'sh',
+      'yaml'
+    },
+
+    init_options = {
+      linters = {
+        rubocop = {
+          command = 'bundle',
+          sourceName = 'rubocop',
+          args = {
+            'exec',
+            'rubocop',
+            '--format',
+            'json',
+            '--force-exclusion',
+            '%filepath'
+          },
+          parseJson = {
+            errorsRoot = 'files[0].offenses',
+            line = 'location.line',
+            column = 'location.column',
+            message = '[${cop_name}]\n${message}',
+            security = 'severity'
+          },
+          securities = {
+            fatal = 'error',
+            warning = 'warning'
+          }
+        },
+        markdownlint = {
+          command = 'markdownlint',
+          sourceName = 'markdownlint',
+          isStderr = true,
+          args = {
+            '--config',
+            '.markdownlint.json',
+            '%filepath'
+          },
+          formatLines = 1,
+          formatPattern = {
+            '^([^:]+):(\\d+):(\\d+)?\\s*(.*)$',
+            {
+              sourceName = 1,
+              sourceNameFilter = true,
+              line = 2,
+              column = 3,
+              message = 4,
+            }
+          },
+          securities = {
+            undefined = 'warning'
+          }
+        },
+        shellcheck = {
+          command = 'shellcheck',
+          debounce = 100,
+          args = {
+            '--format',
+            'json',
+            '-'
+          },
+          sourceName = 'shellcheck',
+          parseJson = {
+            line = 'line',
+            column = 'column',
+            endLine = 'endLine',
+            endColumn = 'endColumn',
+            message = '${message} [${code}]',
+            security = 'level'
+          },
+          securities = {
+            error = 'error',
+            warning = 'warning',
+            info = 'info',
+            style = 'hint'
+          }
+        },
+      },
+
+      filetypes = {
+        markdown = 'markdownlint',
+        ruby = 'rubocop',
+        sh = 'shellcheck',
+      }
+    },
+    on_attach = attacher
+  }
