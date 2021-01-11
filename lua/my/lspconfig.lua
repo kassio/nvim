c273cad287b96c4ab089418a62b45cfa746125dd
Kassio Borges <kassioborgesm@gmail.com>
2021-01-11 13:40:11 +0000

    Fix indenting

--- lua/my/lspconfig.lua
+++ lua/my/lspconfig.lua
@@ -34,94 +34,94 @@ lspconfig.solargraph.setup{
   on_attach = attacher
 }
 
-  lspconfig.diagnosticls.setup{
-    filetypes = {
-      'markdown',
-      'ruby',
-      'sh',
-      'yaml'
-    },
+lspconfig.diagnosticls.setup{
+  filetypes = {
+    'markdown',
+    'ruby',
+    'sh',
+    'yaml'
+  },
 
-    init_options = {
-      linters = {
-        rubocop = {
-          command = 'bundle',
-          sourceName = 'rubocop',
-          args = {
-            'exec',
-            'rubocop',
-            '--format',
-            'json',
-            '--force-exclusion',
-            '%filepath'
-          },
-          parseJson = {
-            errorsRoot = 'files[0].offenses',
-            line = 'location.line',
-            column = 'location.column',
-            message = '[${cop_name}]\n${message}',
-            security = 'severity'
-          },
-          securities = {
-            fatal = 'error',
-            warning = 'warning'
-          }
+  init_options = {
+    linters = {
+      rubocop = {
+        command = 'bundle',
+        sourceName = 'rubocop',
+        args = {
+          'exec',
+          'rubocop',
+          '--format',
+          'json',
+          '--force-exclusion',
+          '%filepath'
         },
-        markdownlint = {
-          command = 'markdownlint',
-          sourceName = 'markdownlint',
-          isStderr = true,
-          args = {
-            '--config',
-            '.markdownlint.json',
-            '%filepath'
-          },
-          formatLines = 1,
-          formatPattern = {
-            '^([^:]+):(\\d+):(\\d+)?\\s*(.*)$',
-            {
-              sourceName = 1,
-              sourceNameFilter = true,
-              line = 2,
-              column = 3,
-              message = 4,
-            }
-          },
-          securities = {
-            undefined = 'warning'
-          }
+        parseJson = {
+          errorsRoot = 'files[0].offenses',
+          line = 'location.line',
+          column = 'location.column',
+          message = '[${cop_name}]\n${message}',
+          security = 'severity'
         },
-        shellcheck = {
-          command = 'shellcheck',
-          debounce = 100,
-          args = {
-            '--format',
-            'json',
-            '-'
-          },
-          sourceName = 'shellcheck',
-          parseJson = {
-            line = 'line',
-            column = 'column',
-            endLine = 'endLine',
-            endColumn = 'endColumn',
-            message = '${message} [${code}]',
-            security = 'level'
-          },
-          securities = {
-            error = 'error',
-            warning = 'warning',
-            info = 'info',
-            style = 'hint'
-          }
-        },
+        securities = {
+          fatal = 'error',
+          warning = 'warning'
+        }
       },
-
-      filetypes = {
-        markdown = 'markdownlint',
-        ruby = 'rubocop',
-        sh = 'shellcheck',
-      }
+      markdownlint = {
+        command = 'markdownlint',
+        sourceName = 'markdownlint',
+        isStderr = true,
+        args = {
+          '--config',
+          '.markdownlint.json',
+          '%filepath'
+        },
+        formatLines = 1,
+        formatPattern = {
+          '^([^:]+):(\\d+):(\\d+)?\\s*(.*)$',
+          {
+            sourceName = 1,
+            sourceNameFilter = true,
+            line = 2,
+            column = 3,
+            message = 4,
+          }
+        },
+        securities = {
+          undefined = 'warning'
+        }
+      },
+      shellcheck = {
+        command = 'shellcheck',
+        debounce = 100,
+        args = {
+          '--format',
+          'json',
+          '-'
+        },
+        sourceName = 'shellcheck',
+        parseJson = {
+          line = 'line',
+          column = 'column',
+          endLine = 'endLine',
+          endColumn = 'endColumn',
+          message = '${message} [${code}]',
+          security = 'level'
+        },
+        securities = {
+          error = 'error',
+          warning = 'warning',
+          info = 'info',
+          style = 'hint'
+        }
+      },
     },
-    on_attach = attacher
-  }
+
+    filetypes = {
+      markdown = 'markdownlint',
+      ruby = 'rubocop',
+      sh = 'shellcheck',
+    }
+  },
+  on_attach = attacher
+}
