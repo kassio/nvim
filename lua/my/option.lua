ca07a2250b182cd9a81cd070076b739b7b3380c7
Kassio Borges <kassioborgesm@gmail.com>
2021-02-14 21:35:45 +0000

    Use `nvim_set_option` instead of the meta-accessors
    This way one doesn't need to be concerned with the option scope (global,
    window, buffer)

--- lua/my/option.lua
+++ lua/my/option.lua
@@ -1,84 +1,74 @@
-local o = vim.o
-local bo = vim.bo
-local wo = vim.wo
+local set = vim.api.nvim_set_option
 
--- Options
-o.autowriteall = true
-o.backup = false
-o.completeopt = 'menuone,noinsert,noselect'
-o.confirm = true
-o.diffopt = 'internal,filler,closeoff,vertical'
-o.fileencodings = 'utf8,nobomb'
-o.fileformats = 'unix,mac'
-o.fillchars = 'vert:│'
-o.guicursor = 'n-v-c-sm:block,i-ci-ve:block,r-cr-o:hor20'
-o.ignorecase = true
-o.inccommand = 'nosplit'
-o.mouse = 'a'
-o.secure = true
-o.sessionoptions = 'winpos,tabpages,help'
-o.shiftround = true
-o.shortmess = 'actToOFI'
-o.showmatch = true
-o.showmode = false
-o.showtabline = 2
-o.smartcase = true
-o.splitbelow = true
-o.splitright = true
-o.switchbuf = 'useopen,usetab,split'
-o.tabline = '%!tabline#update()'
-o.tags = ''
-o.termguicolors = true
-o.title = true
-o.titlestring = "%{substitute(expand('%:p'), $HOME, '$HOME', '')}"
-o.undolevels = 10000
-o.undoreload = 10000
-o.virtualedit = 'block'
-o.wildignorecase = true
-o.wildoptions = 'pum'
-o.writebackup = false
-o.scrolloff = 10
+function setOptions(table)
+  for option, value in pairs(table) do
+    set(option, value)
+  end
+end
 
--- Buffer options
-bo.cindent = true
-bo.complete = '.,w,b,u'
-bo.copyindent = true
-bo.expandtab = true
-bo.infercase = true
-bo.shiftwidth = 2
-bo.smartindent = true
-bo.softtabstop = 2
-bo.spelllang = 'en'
-bo.swapfile = false
-bo.tabstop = 2
-bo.textwidth = 100
-bo.undofile = true
+setOptions{
+  -- Global
+  autowriteall = true,
+  backup = false,
+  completeopt = 'menuone,noinsert,noselect',
+  confirm = true,
+  diffopt = 'internal,filler,closeoff,vertical',
+  fileencodings = 'utf8,nobomb',
+  fileformats = 'unix,mac',
+  fillchars = 'vert:│',
+  guicursor = 'n-v-c-sm:block,i-ci-ve:block,r-cr-o:hor20',
+  ignorecase = true,
+  inccommand = 'nosplit',
+  mouse = 'a',
+  secure = true,
+  sessionoptions = 'winpos,tabpages,help',
+  shiftround = true,
+  shortmess = 'actToOFI',
+  showmatch = true,
+  showmode = false,
+  showtabline = 2,
+  smartcase = true,
+  splitbelow = true,
+  splitright = true,
+  switchbuf = 'useopen,usetab,split',
+  tabline = '%!tabline#update()',
+  tags = '',
+  termguicolors = true,
+  title = true,
+  titlestring = "%{substitute(expand('%:p'), $HOME, '$HOME', '')}",
+  undolevels = 10000,
+  undoreload = 10000,
+  virtualedit = 'block',
+  wildignorecase = true,
+  wildoptions = 'pum',
+  writebackup = false,
+  scrolloff = 10,
 
--- For some reason Buffer options sometimes files if not defined in both
--- buffer and global scopes. ¯\_(ツ)_/¯
-o.cindent = true
-o.complete = '.,w,b,u'
-o.copyindent = true
-o.expandtab = true
-o.infercase = true
-o.shiftwidth = 2
-o.smartindent = true
-o.softtabstop = 2
-o.spelllang = 'en'
-o.swapfile = false
-o.tabstop = 2
-o.textwidth = 100
-o.undofile = true
+  -- Buffer,
+  cindent = true,
+  complete = '.,w,b,u',
+  copyindent = true,
+  expandtab = true,
+  infercase = true,
+  shiftwidth = 2,
+  smartindent = true,
+  softtabstop = 2,
+  spelllang = 'en',
+  swapfile = false,
+  tabstop = 2,
+  textwidth = 100,
+  undofile = true,
 
--- Window options
-wo.colorcolumn = '80,100,120'
-wo.foldenable = false
-wo.foldmethod = 'manual'
-wo.list = true
-wo.listchars = 'tab:> ,trail:-,nbsp:+,precedes:<,extends:>'
-wo.number = true
-wo.numberwidth = 3
-wo.relativenumber = true
-wo.signcolumn = 'yes'
-wo.spell = true
-wo.wrap = false
+  -- Window,
+  colorcolumn = '80,100,120',
+  foldenable = false,
+  foldmethod = 'manual',
+  list = true,
+  listchars = 'tab:> ,trail:-,nbsp:+,precedes:<,extends:>',
+  number = true,
+  numberwidth = 3,
+  relativenumber = true,
+  signcolumn = 'yes',
+  spell = true,
+  wrap = false
+}
