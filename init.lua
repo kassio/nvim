55494d7023132e46edf9f69e55d99b149fe043f8
Kassio Borges <kassioborgesm@gmail.com>
2021-03-18 14:15:34 +0000

    More resilient initialization of packages
    Ensure to load all packages even if one fails.

--- init.lua
+++ init.lua
@@ -1,35 +1,46 @@
-local packer_path = '/site/pack/packer/opt/packer.nvim'
-local execute = vim.api.nvim_command
+local command = vim.cmd
 local fn = vim.fn
-local install_path = fn.stdpath('data')..packer_path
+local packer_path = '/site/pack/packer/opt/packer.nvim'
+local install_path = fn.stdpath('data') .. packer_path
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-  execute('packadd packer.nvim')
+  command('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+  command('packadd packer.nvim')
 end
 
+-- P = Print with inpect
 P = function(anything)
   print(vim.inspect(anything))
 end
 
+-- R = Reload
+-- Reloads a package by name
+-- If it fails to be loaded the error is printed
 R = function(name)
   package.loaded[name] = nil
-  return require(name)
+  local status, result = pcall(require, name)
+
+  if status then
+    return result -- package
+  else
+    print('Failed to load '..name)
+    print(result) -- error message
+  end
 end
 
 -- settings
-require'my/option'
-require'my/global'
-require'my/keymap'
-require'my/autocmd'
-require'my/command'
-require'my/tabline'
+R'my/option'
+R'my/global'
+R'my/keymap'
+R'my/autocmd'
+R'my/command'
+R'my/tabline'
 
 -- libs
-require'my/plugin/completion'
-require'my/plugin/file-tree'
-require'my/plugin/lspconfig'
-require'my/plugin/treesitter'
-require'my/plugin/fuzzyfinder'
-require'my/plugin/statusline'
-require'colorizer'.setup()
+R'my/plugin/completion'
+R'my/plugin/file-tree'
+R'my/plugin/lspconfig'
+R'my/plugin/treesitter'
+R'my/plugin/fuzzyfinder'
+R'my/plugin/statusline'
+R'colorizer'.setup()
