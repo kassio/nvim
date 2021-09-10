bcb1619074d8c312105c1a9316856c71955492a2
Kassio Borges <kassioborgesm@gmail.com>
2021-09-10 01:06:31 +0100

    Use understore for bad spell instead of colors

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,1 +1,19 @@
 vim.cmd('colorscheme palenight')
+
+local hi = function(name, opts)
+  local values = ''
+  for k, v in pairs(opts) do
+    values = string.format('%s %s=%s', values, k, v)
+  end
+
+  vim.cmd(string.format('hi! %s %s', name, values))
+end
+
+local hi_link = function(lhs, rhs)
+  vim.cmd(string.format('hi! link %s %s', lhs, rhs))
+end
+
+hi('SpellBad', { guifg = 'NONE', guibg = 'NONE', gui = 'underline' })
+hi_link('SpellCap', 'SpellBad')
+hi_link('SpellRare', 'SpellBad')
+hi_link('SpellLocal', 'SpellBad')
