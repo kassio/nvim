e16e2b39b0d052f85b4f5fcdc07990aa2c1cbcfb
Kassio Borges <kassioborgesm@gmail.com>
2021-09-11 11:47:33 +0100

    Add :Rspec|RSpec to set/run rspec with test runner

--- lua/plugins/test-runner.lua
+++ lua/plugins/test-runner.lua
@@ -8,3 +8,18 @@ keymap('n', '<leader>ta', ':TestSuite<cr>')
 keymap('n', '<leader>tf', ':TestFile<cr>')
 keymap('n', '<leader>tc', ':TestNearest<cr>')
 keymap('n', '<leader>tr', ':TestLast<cr>')
+
+vim.my.test_runner = {
+  rspec = function()
+    vim.g['test#last_position'] = {
+      file = 'spec/models/file_spec.rb',
+      col = 1,
+      line = 1
+    }
+
+    vim.cmd('TestSuite')
+  end
+}
+
+vim.my.utils.command('RSpec lua vim.my.test_runner.rspec()')
+vim.cmd('cabbrev Rspec RSpec')
