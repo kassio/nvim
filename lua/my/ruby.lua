7f6117c7cca06ab4117e8f0b5b06cfdcfbc27bb5
Kassio Borges <kassioborgesm@gmail.com>
2021-09-16 21:21:49 +0100

    Optmize RubyNamespace code to avoid double iteration

--- lua/my/ruby.lua
+++ lua/my/ruby.lua
@@ -1,34 +1,39 @@
+--[[
+Traverse the AST from the node under the cursor up to the current tree root.
+For each 'module' or 'class' node, get the constants text for the node.
+--]]
 local utils = R('my.utils')
 local ts_utils = R('nvim-treesitter.ts_utils')
 
-local is_namespace = function(node)
+local is_namespace = function(node, extra)
   return vim.tbl_contains({'module', 'class'}, node:type())
 end
 
-function find_root_for(node, root)
-  if node then
-    if is_namespace(node) then
-      root = node
+function namespace_list(current_node, list)
+  list = list or {}
+
+  if current_node then
+    if is_namespace(current_node) then
+      table.insert(list, namespace_constants(current_node))
+      list = vim.tbl_flatten(list)
     end
 
-    return find_root_for(node:parent(), root)
+    return namespace_list(current_node:parent(), list)
   else
-    return root
+    return list
   end
 end
 
-function find_namespace_constants(node, constants)
+function namespace_constants(node, constants)
   constants = constants or {}
 
-  if is_namespace(node) then
-    for i = 0, node:named_child_count() - 1, 1 do
-      local child = node:named_child(i)
+  for i = node:named_child_count() - 1, 0, -1 do
+    local child = node:named_child(i)
 
-      if child:type() == 'constant' then
-        table.insert(constants, vim.treesitter.get_node_text(child, 0))
-      end
-
-      find_namespace_constants(child, constants)
+    if child:type() == 'constant' then
+      table.insert(constants, vim.treesitter.get_node_text(child, 0))
+    elseif child:type() == 'scope_resolution' then
+      namespace_constants(child, constants)
     end
   end
 
@@ -39,10 +44,16 @@ local M = {}
 
 M.namespace = function()
   local node = ts_utils.get_node_at_cursor(0)
-  local root_node = find_root_for(node)
-  local constants = find_namespace_constants(root_node)
+  local constants = namespace_list(node)
+  local namespace = {}
 
-  return table.concat(constants, '::')
+  -- The tree is traversed from bottom-up, that's why
+  -- the list is reversed to build the namespace
+  for i = #constants, 0, -1 do
+    table.insert(namespace, constants[i])
+  end
+
+  return table.concat(namespace, '::')
 end
 
 M.copy_namespace = function(external_clipboard)
