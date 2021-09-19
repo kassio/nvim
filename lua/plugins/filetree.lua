b11e68dfda7bb2c31b0df1bedff505786d3c2d1f
Kassio Borges <kassioborgesm@gmail.com>
2021-09-19 18:04:14 +0100

    Use more nvim_tree defaults

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -6,31 +6,16 @@ g.nvim_tree_width = 32
 g.nvim_tree_git_hl = 1
 g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_indent_markers = 1
+g.nvim_tree_lsp_diagnostics = 1
+g.nvim_tree_highlight_opened_files = 3
 g.nvim_tree_show_icons = {
   git = 1,
   folders = 1,
+  folder_arrows = 1,
   files = 1,
 }
 
-g.nvim_tree_icons = {
-  default = '',
-  symlink = '',
-  git = {
-    unstaged = "*",
-    staged = "✓",
-    unmerged = "",
-    renamed = "➜",
-    untracked = "%"
-  },
-  folder = {
-    default = '▶',
-    open = '▼',
-    empty = '▷',
-    empty_open = '▽',
-    symlink = ""
-  }
-}
-
+g.nvim_tree_gitignore = 0
 g.nvim_tree_ignore = {
   '.git',
   'node_modules',
