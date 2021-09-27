760dd998891af646b4fc49eb8e7f7e298bcf3bc5
Kassio Borges <kassioborgesm@gmail.com>
2021-09-27 11:46:53 +0100

    Update nvim-tree setup config

--- lua/plugins/filetree.lua
+++ lua/plugins/filetree.lua
@@ -1,12 +1,9 @@
 local g = vim.g
 local utils = vim.my.utils
 
-g.nvim_tree_side = 'right'
-g.nvim_tree_width = 32
 g.nvim_tree_git_hl = 1
 g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_lsp_diagnostics = 1
 g.nvim_tree_highlight_opened_files = 3
 g.nvim_tree_show_icons = {
   git = 1,
@@ -24,6 +21,14 @@ g.nvim_tree_ignore = {
   '.vscode',
   '.idea',
 }
+
+require('nvim-tree').setup({
+  view = {
+    width = 32,
+    side = 'right',
+  },
+  lsp_diagnostics = true,
+})
 
 utils.keymap('n', '<leader>p', ':NvimTreeToggle<CR>')
 utils.keymap('n', '<leader>fl', ':NvimTreeFindFile<CR>')
