357de04224b5ad979cb84cef2ddee89fd8ab5798
Kassio Borges <kassioborgesm@gmail.com>
2021-10-06 16:37:33 +0100

    Simplify filetree statusline
    nvim-tree don't expose an API, so the functions used was breaking the
    filetree UI.

--- lua/plugins/statusline/extensions/filetree.lua
+++ lua/plugins/statusline/extensions/filetree.lua
@@ -1,5 +1,3 @@
-local file_tree = R('nvim-tree.lib')
-
 local ensure_path_size = function(path)
   if #path > 28 then
     return vim.fn.pathshorten(path)
@@ -9,14 +7,7 @@ local ensure_path_size = function(path)
 end
 
 local current_line = function()
-  local default_path = vim.fn.getcwd()
-  local node = file_tree.get_node_at_cursor() or {}
-
-  if node.absolute_path then
-    return ensure_path_size(vim.fn.fnamemodify(node.absolute_path, ':.'))
-  else
-    return ensure_path_size(vim.fn.fnamemodify(default_path, ':~'))
-  end
+  return ensure_path_size(vim.fn.fnamemodify(vim.fn.getcwd(), ':~'))
 end
 
 return {
