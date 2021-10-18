1bab35955e5aae5297b9d6b57326c3955f60b52d
Kassio Borges <kassioborgesm@gmail.com>
2021-10-18 18:58:20 +0100

    Fix lspdiagnostic highlights

--- lua/nvim6-fallbacks.lua
+++ lua/nvim6-fallbacks.lua
@@ -21,16 +21,16 @@ end
 -- Fallback to vim.lsp.diagnostic (https://github.com/neovim/neovim/pull/15585)
 -- Default highlight
 Group.new('LspDiagnosticsDefaultError', colors.myError, colors.none)
-Group.new('LspDiagnosticsDefaultWarn', colors.myWarn, colors.none)
-Group.new('LspDiagnosticsDefaultInfo', colors.myInfo, colors.none)
+Group.new('LspDiagnosticsDefaultWarning', colors.myWarn, colors.none)
+Group.new('LspDiagnosticsDefaultInformation', colors.myInfo, colors.none)
 Group.new('LspDiagnosticsDefaultHint', colors.myHint, colors.none)
 -- Signs/Icons highlight
 Group.link('LspDiagnosticsSignError', groups.DiagnosticError)
-Group.link('LspDiagnosticsSignWarn', groups.DiagnosticWarn)
-Group.link('LspDiagnosticsSignInfo', groups.DiagnosticInfo)
+Group.link('LspDiagnosticsSignWarning', groups.DiagnosticWarn)
+Group.link('LspDiagnosticsSignInformation', groups.DiagnosticInfo)
 Group.link('LspDiagnosticsSignHint', groups.DiagnosticHint)
 -- Signs/Icons definition
 sign_define('LspDiagnosticsSignError', vim.my.signs.error)
-sign_define('LspDiagnosticsSignWarn', vim.my.signs.warn)
-sign_define('LspDiagnosticsSignInfo', vim.my.signs.info)
+sign_define('LspDiagnosticsSignWarning', vim.my.signs.warn)
+sign_define('LspDiagnosticsSignInformation', vim.my.signs.info)
 sign_define('LspDiagnosticsSignHint', vim.my.signs.hint)
