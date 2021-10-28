560db16241b999253522977d3ada9d993b4ac21c
Kassio Borges <kassioborgesm@gmail.com>
2021-10-28 20:34:03 +0100

    :nail_care:

--- lua/plugins/fuzzyfinder.lua
+++ lua/plugins/fuzzyfinder.lua
@@ -28,9 +28,9 @@ telescope.setup({
       i = {
         ['<C-j>'] = actions.move_selection_next,
         ['<C-k>'] = actions.move_selection_previous,
-        ['<C-u>'] = { '<c-u>', type = 'command' },
-        ['<C-Down>'] = actions.cycle_history_next,
-        ['<C-Up>'] = actions.cycle_history_prev,
+        ['<C-u>'] = { '<c-u>', type = 'command' }, -- delete inputted text
+        ['<C-p>'] = actions.cycle_history_prev,
+        ['<C-n>'] = actions.cycle_history_next,
       },
     },
     dynamic_preview_title = true,
@@ -60,6 +60,7 @@ vim.my.fuzzyfinder = {
   end,
   oldfiles = builtin.oldfiles,
   treesitter = builtin.treesitter,
+  help_tags = builtin.help_tags,
   builtin = function()
     builtin.builtin({ previewer = false })
   end,
@@ -72,7 +73,7 @@ utils.lua_keymap('n', 'f<c-f>', 'vim.my.fuzzyfinder.builtin()')
 utils.lua_keymap('n', 'f<c-p>', 'vim.my.fuzzyfinder.find_files()')
 utils.lua_keymap('n', 'f<c-g>', 'vim.my.fuzzyfinder.git_files()')
 utils.lua_keymap('n', 'f<c-o>', 'vim.my.fuzzyfinder.oldfiles()')
-utils.lua_keymap('n', 'f<c-h>', 'vim.my.fuzzyfinder.highlights()')
+utils.lua_keymap('n', 'f<c-h>', 'vim.my.fuzzyfinder.help_tags()')
 utils.lua_keymap('n', 'f<c-n>', 'vim.my.fuzzyfinder.current_buffer_fuzzy_find()')
 utils.lua_keymap('n', 'f<c-t>', 'vim.my.fuzzyfinder.treesitter()')
 utils.lua_keymap('n', 'f<c-k>', 'vim.my.fuzzyfinder.buffers()')
