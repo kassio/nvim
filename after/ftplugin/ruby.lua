1ed309c8d4045a7663e29fd96a7cd863fbb430b5
Kassio Borges <kassioborgesm@gmail.com>
2021-10-29 08:57:20 +0100

    Add RubyConvertHashStringKeyToSymbol

--- after/ftplugin/ruby.lua
+++ after/ftplugin/ruby.lua
@@ -2,7 +2,7 @@ local utils = require('my.utils')
 
 local range_cmd = function(line1, line2, command)
   utils.preserve(function()
-    vim.cmd(string.format('silent %s,%s%s', line1, line2, command))
+    vim.cmd(string.format('silent %s,%s%s/e', line1, line2, command))
   end)
 end
 
@@ -16,8 +16,11 @@ vim.my.completion.buffer.completion_sources({
 })
 
 vim.my.ruby = {
-  modernizy_hash_symbol_keys = function(line1, line2)
+  modernizy_hash_symbol_key = function(line1, line2)
     range_cmd(line1, line2, [[s/:\(\w\+\)\s*=>\s*\ze/\1:\ ]])
+  end,
+  convert_hash_string_key_to_symbol = function(line1, line2)
+    range_cmd(line1, line2, [[s/\(['"]\)\([^\1]\{-\}\)\1\s*\(=>\|:\)/\2:]])
   end,
   let_to_var = function(line1, line2)
     range_cmd(line1, line2, [[s/let\%(\w\+\)\?(:\(\w\+\))\s*{\s*\(.\{-\}\)\s*}/\1 = \2]])
@@ -27,6 +30,7 @@ vim.my.ruby = {
   end,
 }
 
-utils.command('-range RubyModernizeHashSymbolKeys lua vim.my.ruby.modernizy_hash_symbol_keys("<line1>", "<line2>")')
+utils.command('-range RubyModernizeHashSymbolKey lua vim.my.ruby.modernizy_hash_symbol_key("<line1>", "<line2>")')
+utils.command('-range RubyConvertHashStringKeyToSymbol lua vim.my.ruby.convert_hash_string_key_to_symbol("<line1>", "<line2>")')
 utils.command('-range RubyLetToVar lua vim.my.ruby.let_to_var("<line1>", "<line2>")')
 utils.command('-range RubyVarToLet lua vim.my.ruby.var_to_let("<line1>", "<line2>")')
