2bdbf013028d3d8299b4e19c9d84cfcee95090dd
Kassio Borges <kassioborgesm@gmail.com>
2021-11-19 00:36:47 +0000

    more golang tweaks

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -4,3 +4,7 @@ vim.opt_local.softtabstop = 4
 vim.opt_local.expandtab = false
 
 vim.cmd('Tmap clear; go run %')
+
+vim.my.utils.augroup('user:ftplugin:go', {
+  { 'BufWritePre,FileWritePre', '*.go', 'lua vim.lsp.buf.formatting()' }
+})
