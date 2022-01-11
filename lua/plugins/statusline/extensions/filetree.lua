24a4d146024bae9fcafdc75437a0a043e97d457a
Kassio Borges <kassioborgesm@gmail.com>
2022-01-11 17:02:53 +0000

    Use current line as statuline in the filetree

--- lua/plugins/statusline/extensions/filetree.lua
+++ lua/plugins/statusline/extensions/filetree.lua
@@ -1,13 +1,5 @@
-local ensure_path_size = function(path)
-  if #path > 28 then
-    return vim.fn.pathshorten(path)
-  else
-    return path
-  end
-end
-
 local current_line = function()
-  return ensure_path_size(vim.fn.fnamemodify(vim.fn.getcwd(), ':~'))
+  return vim.fn.getline('.'):gsub('[^%a%d%p]', '')
 end
 
 return {
