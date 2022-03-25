b5a803991ad8205f128895eb1bde604c1d428137
Kassio Borges <kassioborgesm@gmail.com>
2022-03-25 12:43:06 +0000

    Avoid errors when closing floating windows

--- lua/my/windows.lua
+++ lua/my/windows.lua
@@ -2,9 +2,9 @@ local M = {}
 
 M.close_floating = function()
   for _, win in ipairs(vim.api.nvim_list_wins()) do
-    local config = vim.api.nvim_win_get_config(win)
+    local ok, config = pcall(vim.api.nvim_win_get_config, win)
 
-    if config.relative ~= '' then
+    if ok and config.relative ~= '' then
       vim.api.nvim_win_close(win, true)
     end
   end
