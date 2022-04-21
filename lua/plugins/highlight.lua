b01eab788da93407bbf133a24fe265e2b0cecc4e
Kassio Borges <kassioborgesm@gmail.com>
2022-04-21 15:31:25 +0200

    Simplify highlight code

--- lua/plugins/highlight.lua
+++ lua/plugins/highlight.lua
@@ -1,4 +1,5 @@
 local theme = vim.my.theme
+local colors = theme.colors
 local hl_def = vim.my.utils.highlight_define
 local hl_extend = vim.my.utils.highlight_extend
 local sign_define = vim.my.utils.sign_define
@@ -15,35 +16,42 @@ require('pqf').setup({
   },
 })
 
+local attrs = function(opts)
+  return vim.tbl_extend('keep', opts, {
+    foreground = 'NONE',
+    background = 'NONE',
+    sp = 'NONE',
+  })
+end
+
 vim.cmd('colorscheme ' .. theme.colorscheme)
-vim.opt.background = vim.o.background
 
 -- Spell
-hl_def('SpellBad', { foreground = 'NONE', background = 'NONE', undercurl = true })
+hl_def('SpellBad', attrs({ underline = true, sp = colors.warn }))
 hl_extend('SpellCap', 'SpellBad')
 hl_extend('SpellRare', 'SpellBad')
 hl_extend('SpellLocal', 'SpellBad')
 
 -- Spacing/Visual clues
-hl_def('ColorColumn', { foreground = 'NONE', background = theme.colors.shadow })
+hl_def('ColorColumn', attrs({ background = colors.shadow }))
 
-hl_def('NonText', { foreground = theme.colors.shadow, background = 'NONE' })
+hl_def('NonText', attrs({ foreground = colors.shadow }))
 hl_extend('Whitespace', 'NonText')
 
-hl_def('VertSplit', { foreground = theme.colors.ignore })
-hl_def('SpecialKey', { foreground = theme.colors.shadow })
+hl_def('VertSplit', { foreground = colors.ignore })
+hl_def('SpecialKey', { foreground = colors.shadow })
 hl_extend('SpecialChar', 'SpecialKey')
 hl_extend('EndOfBuffer', 'SpecialKey')
 
 -- matching parantheses/blocks marks
-hl_def('MatchParen', { foreground = 'NONE', background = 'NONE', bold = true })
+hl_def('MatchParen', attrs({ bold = true }))
 
 -- Diagnostics (vim.diagnostic)
 -- Default highlight
-hl_def('DiagnosticError', { foreground = theme.colors.error, background = 'NONE' })
-hl_def('DiagnosticWarn', { foreground = theme.colors.warn, background = 'NONE' })
-hl_def('DiagnosticInfo', { foreground = theme.colors.info, background = 'NONE' })
-hl_def('DiagnosticHint', { foreground = theme.colors.hint, background = 'NONE' })
+hl_def('DiagnosticError', attrs({ foreground = colors.error }))
+hl_def('DiagnosticWarn', attrs({ foreground = colors.warn }))
+hl_def('DiagnosticInfo', attrs({ foreground = colors.info }))
+hl_def('DiagnosticHint', attrs({ foreground = colors.hint }))
 
 -- Signs/Icons definition
 sign_define('DiagnosticSignError', theme.signs.error, 'DiagnosticError')
@@ -51,37 +59,31 @@ sign_define('DiagnosticSignWarn', theme.signs.warn, 'DiagnosticWarn')
 sign_define('DiagnosticSignInfo', theme.signs.info, 'DiagnosticInfo')
 sign_define('DiagnosticSignHint', theme.signs.hint, 'DiagnosticHint')
 
--- Default escape string
-hl_extend('TSStringEscape', 'TSString', { bold = true })
-hl_extend('TSStringSpecial', 'TSString', { bold = true })
-
-hl_def('TSError', { background = 'NONE', undercurl = true, italic = true })
-
 -- Git
-hl_def('GitSignsCurrentLineBlame', {
-  foreground = theme.colors.shadow,
-  background = 'NONE',
-  italic = true,
-})
+hl_def('GitSignsCurrentLineBlame', attrs({ foreground = colors.shadow, italic = true }))
 
-hl_def('GitSignAdd', { foreground = theme.colors.hint, background = 'NONE' })
-hl_def('GitSignChange', { foreground = theme.colors.warn, background = 'NONE' })
-hl_def('GitSignDelete', { foreground = theme.colors.error, background = 'NONE' })
+hl_def('GitSignAdd', attrs({ foreground = colors.hint }))
+hl_def('GitSignChange', attrs({ foreground = colors.warn }))
+hl_def('GitSignDelete', attrs({ foreground = colors.error }))
 
-hl_def('GitSignAddLineNr', { foreground = theme.colors.hint, background = 'NONE' })
-hl_def('GitSignChangeLineNr', { foreground = theme.colors.warn, background = 'NONE' })
-hl_def('GitSignDeleteLineNr', { foreground = theme.colors.error, background = 'NONE' })
+hl_def('GitSignAddLineNr', attrs({ foreground = colors.hint }))
+hl_def('GitSignChangeLineNr', attrs({ foreground = colors.warn }))
+hl_def('GitSignDeleteLineNr', attrs({ foreground = colors.error }))
 
 -- Filetree
-hl_def('NvimTreeOpenedFile', { foreground = 'NONE', background = 'NONE', undercurl = true })
+hl_def('NvimTreeOpenedFile', attrs({ underline = true, sp = colors.info }))
 
 -- Treesitter
-hl_def('TSDefinition', { foreground = 'NONE', background = theme.colors.shadow })
+hl_def('TSError', attrs({ undercurl = true, italic = true, sp = colors.error }))
+-- globals
+hl_def('TSDefinition', attrs({ background = colors.shadow }))
 hl_extend('TSDefinitionUsage', 'TSDefinition')
 hl_extend('TSTypeBuiltin', 'Type')
 hl_extend('TSVariable', 'Normal')
 hl_extend('TSParameter', 'Normal')
 hl_extend('TSFuncBuiltin', 'Identifier')
+hl_extend('TSStringEscape', 'TSString', { bold = true })
+hl_extend('TSStringSpecial', 'TSString', { bold = true })
 
 -- Ruby with Treesitter
 hl_extend('rubyTSType', 'rubyconstant')
