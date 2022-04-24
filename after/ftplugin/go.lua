6b815c8ea86286e945b1fcd7b5d475ccce4ca2de
Kassio Borges <kassioborgesm@gmail.com>
2022-04-24 22:28:34 +0200

    Add go specific debugging command/keymaps

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -4,3 +4,6 @@ vim.opt_local.softtabstop = 4
 vim.opt_local.expandtab = false
 
 vim.cmd('Tmap go run %')
+
+vim.api.nvim_create_user_command('GoDebugTest', require('dap-go').debug_test, {})
+vim.keymap.set('n', '<leader>dT', '<cmd>GoDebugTest<cr>')
