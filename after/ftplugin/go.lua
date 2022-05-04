a0b5bc166ab8553dea2732627f1b0475bb33eedd
Kassio Borges <kassioborgesm@gmail.com>
2022-05-04 15:43:32 +0200

    refactoring debugger config

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -5,5 +5,8 @@ vim.opt_local.expandtab = false
 
 vim.cmd('Tmap go run %')
 
-vim.api.nvim_create_user_command('GoDebugTest', require('dap-go').debug_test, {})
-vim.keymap.set('n', '<leader>dT', '<cmd>GoDebugTest<cr>')
+local dap_go = require('dap-go')
+dap_go.setup()
+
+vim.api.nvim_create_user_command('GoDebugTest', dap_go.debug_test, {})
+vim.keymap.set('n', '<leader>dt', '<cmd>GoDebugTest<cr>')
--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -3,7 +3,6 @@ local dap = require('dap')
 local widgets = require('dap.ui.widgets')
 local dapui = require('dapui')
 
-require('dap-go').setup()
 require('nvim-dap-virtual-text').setup()
 
 dapui.setup()
@@ -17,38 +16,27 @@ dap.listeners.before.event_exited['dapui_config'] = function()
   dapui.close()
 end
 
-local dap_cmd = function(name, fn)
-  vim.api.nvim_create_user_command('Dap' .. name, fn, {})
+local dap_cmd = function(fn, action, map)
+  local name = 'Dap' .. action
+  vim.api.nvim_create_user_command(name, fn, {})
+
+  if map ~= nil then
+    keymap('n', map, '<cmd>' .. name .. '<cr>')
+  end
 end
 
-local conditional_breakpoint = function()
+dap_cmd(dap.continue, 'Continue', '<F5>')
+dap_cmd(dap.step_over, 'StepOver', '<F6>')
+dap_cmd(dap.step_into, 'StepInto', '<F7>')
+dap_cmd(dap.step_out, 'StepOut', '<F8>')
+dap_cmd(widgets.hover, 'Hover', '<leader>dk')
+dap_cmd(dap.repl.open, 'REPL', '<leader>dr')
+dap_cmd(dap.run_last, 'RunLast', '<leader>dl')
+dap_cmd(dap.toggle_breakpoint, 'ToggleBreakpoint', '<leader>db')
+dap_cmd(dapui.toggle, 'ToggleUI', '<leader>du')
+dap_cmd(function()
   dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
-end
-
-local log_breakpoint = function()
+end, 'SetBreakpointCondition', '<leader>dB')
+dap_cmd(function()
   dap.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))
-end
-
-dap_cmd('ToggleUI', dapui.toggle)
-dap_cmd('REPL', dap.repl.open)
-dap_cmd('RunLast', dap.run_last)
-dap_cmd('Continue', dap.continue)
-dap_cmd('StepOver', dap.step_over)
-dap_cmd('StepInto', dap.step_into)
-dap_cmd('StepOut', dap.step_out)
-dap_cmd('ToggleBreakpoint', dap.toggle_breakpoint)
-dap_cmd('SetBreakpointCondition', conditional_breakpoint)
-dap_cmd('SetBreakpointLog', log_breakpoint)
-dap_cmd('Hover', widgets.hover)
-
-keymap('n', '<F5>', '<cmd>DapContinue<cr>')
-keymap('n', '<F6>', '<cmd>DapStepOver<cr>')
-keymap('n', '<F7>', '<cmd>DapStepInto<cr>')
-keymap('n', '<F8>', '<cmd>DapStepOut<cr>')
-keymap('n', '<leader>dt', '<cmd>DapToggleUI<cr>')
-keymap('n', '<leader>dr', '<cmd>DapREPL<cr>')
-keymap('n', '<leader>dl', '<cmd>DapRunLast<cr>')
-keymap('n', '<leader>db', '<cmd>DapToggleBreakpoint<cr>')
-keymap('n', '<leader>dB', '<cmd>DapSetBreakpointCondition<cr>')
-keymap('n', '<leader>dp', '<cmd>DapSetBreakpointLog<cr>')
-keymap('n', '<leader>dk', '<cmd>DapHover<cr>')
+end, 'SetBreakpointLog', '<leader>dp')
